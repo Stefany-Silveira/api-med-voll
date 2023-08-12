ed7c23ae9a1bcc5ae2d1bce8d10b6403e148a82d
stefanysilveira <stefany.silveira21@gmail.com>
2023-08-12 13:16:54 -0300

    add Classe Service

--- src/main/java/com/med/voll/controller/ConsultaController.java
+++ src/main/java/com/med/voll/controller/ConsultaController.java
@@ -1,9 +1,11 @@
 package com.med.voll.controller;
 
+import com.med.voll.domain.consulta.AgendaDeConsultas;
 import com.med.voll.domain.consulta.DadosAgendamentoConsulta;
 import com.med.voll.domain.consulta.DadosDetalhamentoConsulta;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,10 +16,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("consultas")
 public class ConsultaController {
 
+    @Autowired
+    private AgendaDeConsultas agenda;
+
     @PostMapping
     @Transactional
     public ResponseEntity agendar(@RequestBody @Valid DadosAgendamentoConsulta dados) {
-        System.out.println(dados);
+       agenda.agendar(dados);
         return ResponseEntity.ok(new DadosDetalhamentoConsulta(null, null, null, null));
     }
 
