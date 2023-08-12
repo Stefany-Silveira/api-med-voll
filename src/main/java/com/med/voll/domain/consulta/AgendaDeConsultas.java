9a5729f3115f8a7c53e51b8945ed6eddf2e6dc13
stefanysilveira <stefany.silveira21@gmail.com>
2023-08-12 15:43:18 -0300

    add escolha de médico aleatório

--- src/main/java/com/med/voll/domain/consulta/AgendaDeConsultas.java
+++ src/main/java/com/med/voll/domain/consulta/AgendaDeConsultas.java
@@ -37,5 +37,14 @@ public class AgendaDeConsultas {
     }
 
     private Medico escolherMedico(DadosAgendamentoConsulta dados) {
+
+        if (dados.idMedico() != null) {
+            return medicoRepository.getReferenceById(dados.idMedico());
+        }
+
+        if (dados.especialidade() == null) {
+            throw new ValidacaoException("Especialidade é obrigatória quando médico não for escolhido");
+        }
+        return medicoRepository.escolherMedicoAleatorio(dados.especialidade(), dados.data());
     }
 }
